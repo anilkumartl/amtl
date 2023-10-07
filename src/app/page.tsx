fd18ae4ae1445eea3d2aafcd8df89cf8932e6b3c
anilkumartl <anilgowdatl@gmail.com>
2023-10-07 11:27:49 +0530

    pnpm error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By
             <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
           </a>
         </div>
